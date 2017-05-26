f68ae7e90a0ea436a544229b5b70cc2767cc2a79
canlas1 <dc@uscr.com>
2017-05-25 21:23:26 -0400

    initial commit to my branch

---
+++ config/auth.js
@@ -0,0 +1,14 @@
+var keys = require("./client_id");
+module.exports = {
+	'facebookAuth' : {
+		'clientID': 'enter client id here',
+		'clientSecret': 'enter client secret here',
+		'callbackURL': 'enter callback here'
+	},
+
+	'googleAuth' : {
+		'clientID': keys.client_id,
+		'clientSecret': keys.client_secret,
+		'callbackURL': "https://teamprojecttwo.herokuapp.com/auth/google/callback"
+	}
+}
