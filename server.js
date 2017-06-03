1059283e51e8df4e4be48759169034f54b40fc96
canlas1 <dc@uscr.com>
2017-06-03 10:09:11 -0400

    pushing updates for beer form and forgot password

--- server.js
+++ server.js
@@ -64,12 +64,6 @@
    // will redirect the user back to this application at /auth/google/callback
  app.get('/auth/google',
    passport.authenticate('google', { scope: ['openid email profile'] }));
-
-//GET /auth/google/callback
-// Use passport.authenticate() as route middleware to authenticate the
-//request.  If authentication fails, the user will be redirected back to the
-//    login page.  Otherwise, the primary route function function will be called,
- //   which, in this example, will redirect the user to the home page.
  
  app.get('/auth/google/callback',
    passport.authenticate('google', {
@@ -79,12 +73,6 @@
      // Authenticated successfully
      res.redirect('/');
    });
-
- // app.get('/account', ensureAuthenticated, function(req, res) {
- //   res.render('account', {
- //    user: req.user
- //   });
- // });
 
  app.get('/logout', function(req, res){
   console.log('logging out');
