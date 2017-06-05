2f82ca9a4e9d68f66e6fac2d8bb15f0b65aa06e4
canlas1 <dc@uscr.com>
2017-06-04 21:56:23 -0400

    adjusted custom css with text shadows and changed body background

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-    var express    = require('express')
-    var app        = express()
-    var passport   = require('passport')
-    var session    = require('express-session')
+    var express = require('express')
+    var app = express()
+    var passport = require('passport')
+    var session = require('express-session')
     var bodyParser = require('body-parser')
-    var env        = require('dotenv').load()
-    var exphbs     = require('express-handlebars')
-    var path       = require("path")
+    var env = require('dotenv').load()
+    var exphbs = require('express-handlebars')
+    var path = require("path")
     var PORT = process.env.PORT || 3000;
     var passport = require('passport');
     var Auth0Strategy = require('passport-auth0');
@@ -13,10 +13,7 @@
     var dotenv = require('dotenv');
     var logger = require('morgan');
 
-    
-    // var GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-    
     // Express Static
     app.use(express.static(path.join(__dirname, "app/public")));
 
@@ -26,20 +23,20 @@
 
 
 
-     // For Passport
-    app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+    // For Passport
+    app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true })); // session secret
     app.use(passport.initialize());
     app.use(passport.session()); // persistent login sessions
 
 
-     //For Handlebars
+    //For Handlebars
     app.set('views', path.join(__dirname, 'views'));
-    app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+    app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
     app.set('view engine', 'handlebars');
-    
 
-    app.get('/', function(req, res){
-      res.render('index');
+
+    app.get('/', function(req, res) {
+        res.render('index');
     });
 
     // //For Google Strategy
@@ -54,7 +51,7 @@
     // clientSecret: "pP2DhQC2dj15uLgLklLmqZ60",
     // callbackURL: "https://teambeerlog.herokuapp.com/oauth2/callback"
 
-  // },
+    // },
 
     // function(accessToken, refreshToken, profile, cb) {
     // User.findOrCreate({ googleId: profile.id },function (err, user) {
@@ -64,58 +61,53 @@
     // ));
 
     //  GET /auth/google
-   // Use passport.authenticate() as route middleware to authenticate the
-   // request.  The first step in Google authentication will involve
-   // redirecting the user to google.com.  After authorization, Google
-   // will redirect the user back to this application at /auth/google/callback
- // app.get('/auth/google',
- //   passport.authenticate('google', { scope: ['openid email profile'] }));
- 
- // app.get('/auth/google/callback',
- //   passport.authenticate('google', {
- //     failureRedirect: '/login'
- //   }),
- //   function(req, res) {
- //     // Authenticated successfully
- //     res.redirect('/');
- //   });
+    // Use passport.authenticate() as route middleware to authenticate the
+    // request.  The first step in Google authentication will involve
+    // redirecting the user to google.com.  After authorization, Google
+    // will redirect the user back to this application at /auth/google/callback
+    // app.get('/auth/google',
+    //   passport.authenticate('google', { scope: ['openid email profile'] }));
 
- app.get('/logout', function(req, res){
-  console.log('logging out');
-  req.logout();
-  res.redirect('/');
-});
+    // app.get('/auth/google/callback',
+    //   passport.authenticate('google', {
+    //     failureRedirect: '/login'
+    //   }),
+    //   function(req, res) {
+    //     // Authenticated successfully
+    //     res.redirect('/');
+    //   });
+
+    app.get('/logout', function(req, res) {
+        console.log('logging out');
+        req.logout();
+        res.redirect('/');
+    });
 
     //Models
     var models = require("./app/models");
 
-
     //Routes
-    var authRoute = require('./app/routes/auth.js')(app,passport);
+    var authRoute = require('./app/routes/auth.js')(app, passport);
     var beerpost = require('./app/routes/beerpost.js')(app);
-    
+
 
 
     //load passport strategies
-    require('./app/config/passport/passport.js')(passport,models.user,models.brewers);
+    require('./app/config/passport/passport.js')(passport, models.user, models.brewers);
 
     //Sync Database
-    models.sequelize.sync().then(function(){
-    console.log('Nice! Database looking good!')
+    models.sequelize.sync().then(function() {
+        console.log('Nice! Database looking good!')
 
-    }).catch(function(err){
-    console.log(err,"Something went wrong with the Database Update!")
+    }).catch(function(err) {
+        console.log(err, "Something went wrong with the Database Update!")
     });
 
 
 
-    app.listen(PORT, function(err){
-        if(!err)
-        console.log("Live on Port " + PORT ); 
+    app.listen(PORT, function(err) {
+        if (!err)
+            console.log("Live on Port " + PORT);
         else console.log(err)
 
     });
-
-
-
-    
