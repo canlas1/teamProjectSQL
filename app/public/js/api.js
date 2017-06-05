5418ff1c8eaa4393bec056740a3626ebed7ff822
canlas1 <dc@uscr.com>
2017-06-05 15:23:50 -0400

    removed api.js

--- app/public/js/api.js
+++ app/public/js/api.js
@@ -1,18 +1,18 @@
-console.log('Connection succesful');
-var baseURL = "http://api.brewerydb.com/v2/search?q=Goosinator&type=beer";
-var apiKey = "&key=4191d2d412141ffc3b1cb2e8ea798f7f";
-var withBrewery = "&withBreweries=Y";
-var input = document.getElementById("input");
-var button = document.getElementById("button");
+// console.log('Connection succesful');
+// var baseURL = "http://api.brewerydb.com/v2/search?q=Goosinator&type=beer";
+// var apiKey = "&key=4191d2d412141ffc3b1cb2e8ea798f7f";
+// var withBrewery = "&withBreweries=Y";
+// var input = document.getElementById("input");
+// var button = document.getElementById("button");
 
-$(button).on("click", function() {
-  console.log("Button clicked");
-  console.log(input.value);
+// $(button).on("click", function() {
+//   console.log("Button clicked");
+//   console.log(input.value);
 
-  var queryURL = baseURL + withBrewery + apiKey;
-  console.log(queryURL);
+//   var queryURL = baseURL + withBrewery + apiKey;
+//   console.log(queryURL);
 
-  $.get(queryURL, function(data, status) {
-    console.log("Data: " + data + "\nStatus: " + status);
-  });
-});
+//   $.get(queryURL, function(data, status) {
+//     console.log("Data: " + data + "\nStatus: " + status);
+//   });
+// });
