e7ffdfaea2e234bda4f900c977661a533c10b042
WinDeck <windeck@WinDecks-MacBook-Pro.local>
2017-06-05 17:11:06 -0400

    resolving conflicts

--- app/models/user.js
+++ app/models/user.js
@@ -2,68 +2,55 @@ module.exports = function(sequelize, Sequelize) {
 
     var User = sequelize.define('user', {
 
-            id: {
-                autoIncrement: true,
-                primaryKey: true,
-                type: Sequelize.INTEGER
-            },
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER
+        },
 
-            firstname: {
-                type: Sequelize.STRING,
-                notEmpty: true
-            },
+        firstname: {
+            type: Sequelize.STRING,
+            notEmpty: true
+        },
 
-            lastname: {
-                type: Sequelize.STRING,
-                notEmpty: true
-            },
+        lastname: {
+            type: Sequelize.STRING,
+            notEmpty: true
+        },
 
-            email: {
-                type: Sequelize.STRING,
-                validate: {
-                    isEmail: true
-                }
-            },
-            password: {
-                type: Sequelize.STRING,
-                allowNull: false
-
-            },
-            last_login: {
-                type: Sequelize.DATE,
-                defaultValue: Sequelize.NOW
-            },
-
-            status: {
-                type: Sequelize.ENUM('active', 'inactive'),
-                defaultValue: 'active'
+        email: {
+            type: Sequelize.STRING,
+            validate: {
+                isEmail: true
             }
+        },
+        password: {
+            type: Sequelize.STRING,
+            allowNull: false
 
-        }, {
-            classMethods: {
-                associate: function(models) {
-                    // User.hasMany(models.beer);
-        User.belongsToMany(models.beer, {through: "userbeer"});
+        },
+        last_login: {
+            type: Sequelize.DATE,
+            defaultValue: Sequelize.NOW
+        },
 
-<<<<<<< HEAD
-                }
-            }
-        });
-=======
         status: {
             type: Sequelize.ENUM('active', 'inactive'),
             defaultValue: 'active'
-        },
+        }
 
-        // classMethods: {
-        //     associate: function(models) {
-        //         Burger.hasOne(models.Customer);
-        //     }
-        // }
+    }, {
+        classMethods: {
+            associate: function(models) {
+                // User.hasMany(models.beer);
+                User.belongsToMany(models.beer, { through: "userbeer" });
 
+
+            }
+        }
     });
->>>>>>> b03a4b9c9c202f4c6da64d98793474899cac05af
 
-return User;
+
+    return User;
 
 }
