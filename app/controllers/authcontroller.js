87669db5747817bd5fbce9aa9c6b49b9067ec687
canlas1 <dc@uscr.com>
2017-06-06 01:16:45 -0400

    pushing it all to the master

--- app/controllers/authcontroller.js
+++ app/controllers/authcontroller.js
@@ -6,51 +6,29 @@ var exports = module.exports = {}
 
 exports.signup = function(req,res){
 
-	res.render('signup'); 
+  res.render('signup'); 
 
 }
 
 exports.forgot = function(req,res){
 
-	res.render('forgot'); 
+  res.render('forgot'); 
 
 }
 
 exports.signin = function(req,res){
 
-	res.render('signin'); 
+  res.render('signin'); 
 
 }
 
 exports.dashboard = function(req,res){
-  var userId = req.session.passport.user;
-  var username = "";
 
-  db.user.findOne({
-    where: {
-      id: userId
-    }
-  }).then(function(user){
-    var user = user.get();
-    username = user.username
-      console.log("USER",username);
-
-  })
-  // console.log("usrname", req.user.username);
-  // console.log("displayName", req.session.passport);
   db.beer.findAll({
       // include: [db.beer]
     }).then(function(dbBeer) {
-<<<<<<< HEAD
       console.log(dbBeer);
     return res.render("dashboard", dbBeer);
-=======
-      var hbsObject = {
-      beer: dbBeer,
-      username: username
-    };
-    return res.render("dashboard", hbsObject);
->>>>>>> 26979948b5bcad5fc3cd65cc7a00751fc06f5898
 
     });
 
@@ -58,19 +36,12 @@ exports.dashboard = function(req,res){
 
 exports.beer = function(req,res){
 
-	db.beer.findAll({
+  db.beer.findAll({
       // include: [db.beer]
     }).then(function(dbBeer) {
-    	var hbsObject = {
+      var hbsObject = {
       beer: dbBeer
     };
-
-  // db.user.findAll({
-  //     // include: [db.beer]
-  //   }).then(function(dbUser) {
-  //     var userObject = {
-  //     user: dbUser
-  //   };
     return res.render("beer", hbsObject);
 
     });
@@ -78,7 +49,7 @@ exports.beer = function(req,res){
 }
 
 exports.logout = function(req,res){
-	console.log(req.session);
+  console.log(req.session);
   req.session.destroy(function(err) {
   res.redirect('/');
   });
@@ -86,5 +57,5 @@ exports.logout = function(req,res){
 }
 
 exports.addBeer = function(req,res) {
-	res.render('dashboard');
+  res.render('dashboard');
 }
